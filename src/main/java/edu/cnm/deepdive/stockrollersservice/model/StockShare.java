78632405a667c022a8941e6a87a9c265756a98be
Andrew Patten <andpatten@gmail.com>
2019-10-15 15:58:28 -0600

    Updated User class.

--- src/main/java/edu/cnm/deepdive/stockrollersservice/model/StockShare.java
+++ src/main/java/edu/cnm/deepdive/stockrollersservice/model/StockShare.java
@@ -1,5 +1,11 @@
 package edu.cnm.deepdive.stockrollersservice.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class StockShare {
+
+  @Id
 
 }
--- src/main/java/edu/cnm/deepdive/stockrollersservice/model/User.java
+++ src/main/java/edu/cnm/deepdive/stockrollersservice/model/User.java
@@ -3,9 +3,13 @@ package edu.cnm.deepdive.stockrollersservice.model;
 import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import org.springframework.lang.NonNull;
 
 @Entity
@@ -15,6 +19,12 @@ public class User {
   @GeneratedValue(strategy = GenerationType.AUTO)
   @Column(name = "user_id", updatable = false, nullable = false)
   private Long id;
+
+  @ManyToMany(fetch = FetchType.LAZY)
+  @JoinTable(name = "user_follow",
+  joinColumns = @JoinColumn(name = "follower_id"),
+      inverseJoinColumns = @JoinColumn(name = "followed_id")
+  )
 
   @NonNull
   @Column(nullable = false, updatable = false)
