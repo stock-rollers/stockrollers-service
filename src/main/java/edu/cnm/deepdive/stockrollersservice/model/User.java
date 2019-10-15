ce39ba1bd6450745696416064618d7ae34c1ddb8
Andrew Patten <andpatten@gmail.com>
2019-10-15 15:09:39 -0600

    Implemented User entity.

--- src/main/java/edu/cnm/deepdive/stockrollersservice/model/User.java
+++ src/main/java/edu/cnm/deepdive/stockrollersservice/model/User.java
@@ -1,5 +1,54 @@
 package edu.cnm.deepdive.stockrollersservice.model;
 
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import org.springframework.lang.NonNull;
+
+@Entity
 public class User {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(name = "user_id", updatable = false, nullable = false)
+  private Long id;
+
+  @NonNull
+  @Column(nullable = false, updatable = false)
+  private List<Stock> favorites;
+
+  @NonNull
+  @Column(name = "preferred", nullable = false, updatable = false)
+  List<Industry> preferred;
+
+  @NonNull
+  @Column(name = "stock_name", nullable = false, updatable = false)
+  private String stockName;
+
+  public List<Stock> getFavorites() {
+    return favorites;
+  }
+
+  public void setFavorites(List<Stock> favorites) {
+    this.favorites = favorites;
+  }
+
+  public List<Industry> getPreferred() {
+    return preferred;
+  }
+
+  public void setPreferred(List<Industry> preferred) {
+    this.preferred = preferred;
+  }
+
+  public String getStockName() {
+    return stockName;
+  }
+
+  public void setStockName(String stockName) {
+    this.stockName = stockName;
+  }
 }
